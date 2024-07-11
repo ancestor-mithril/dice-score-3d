2c195b81e269d85d381d95074021a0cda2eff601
ancestor-mithril <sgeorge.sstoica99@gmail.com>
2024-07-11 13:20:01 +0300

    Reorganized readers

--- dice_score_3d/readers.py
+++ dice_score_3d/readers.py
@@ -1,27 +1,41 @@
 import SimpleITK as sitk
-import nibabel as nib
+import nibabel
 import numpy as np
+from nibabel import io_orientation
 
 
-def read_sitk(path):
-    return sitk.GetArrayFromImage(sitk.ReadImage(path))
+class ReadError(Exception):
+    pass
 
 
-def read_nibabel(path):
-    return nib.load(path).get_fdata()
-
-
-def robust_read(path):
+def read_sitk(path: str, reorient: bool):
     try:
-        return read_sitk(path)
-    except RuntimeError:
-        # ITK ERROR: ITK only supports orthonormal direction cosines.  No orthonormal definition found!
-        pass
-    return read_nibabel(path)
+        img = sitk.ReadImage(path)
+    except RuntimeError as e:
+        # "ITK ERROR: ITK only supports orthonormal direction cosines.  No orthonormal definition found!"
+        # Sometimes SimpleITK just refuses to read invalid volumes. In this case, we try to use nibabel
+        raise ReadError('SimpleITK reading error') from e
+    if reorient:
+        img = sitk.DICOMOrient(img)
+    return sitk.GetArrayFromImage(img)
 
 
-def read_mask(path):
-    mask = robust_read(path)
-    if mask.dtype in (np.uint8, np.uint16, np.int8, np.int16):
-        return mask
-    return mask.astype(np.uint8 if np.max(mask) < 255 else np.uint16)
+def read_nibabel(path: str, reorient: bool):
+    img = nibabel.load(path)
+    if reorient:
+        img = img.as_reoriented(io_orientation(img.affine))
+    return img.get_fdata()
+
+
+def robust_read(path: str, reorient: bool):
+    try:
+        return read_sitk(path, reorient)
+    except ReadError as e1:
+        try:  # We try to read with nibabel
+            return read_nibabel(path, reorient)
+        except Exception as e2:
+            raise e2 from e1  # we raise both exceptions
+
+
+def read_mask(path: str, reorient: bool, dtype: np.dtype):
+    return robust_read(path, reorient).astype(dtype, copy=False)
